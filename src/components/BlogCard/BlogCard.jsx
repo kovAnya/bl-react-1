ff5a4fc50c185a79dfe2e01fbd7a11e7e28078d3
Anna Kovalchuk <annazukh@gmail.com>
2022-10-22 15:31:45 +0300

    task-1

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -1,3 +1,30 @@
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import { Card, CardHeader, CardPoster, CardBody, Tag, CardTitle, CardText, CardFooter, UserBox, Avatar, UserInfo, UserName, Date } from './BlogCard.styled';
+
+export const BlogCard = ({poster, tag, title, description, userName, avatar, postedAt}) => {
+  return <div>BlogCard
+    <Card>
+  <CardHeader>
+    <CardPoster
+      src={poster}
+      alt={userName}
+    />
+  </CardHeader>
+  <CardBody>
+    <Tag>{tag}</Tag>
+        <CardTitle>{title}</CardTitle>
+    <CardText>
+      {description}
+    </CardText>
+  </CardBody>
+  <CardFooter>
+    <UserBox>
+      <Avatar src={avatar} alt={userName} />
+      <UserInfo>
+        <UserName>{userName}</UserName>
+            <Date>{postedAt}</Date>
+      </UserInfo>
+    </UserBox>
+  </CardFooter>
+</Card>
+  </div>;
 };
