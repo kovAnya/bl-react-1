bbd40961d7c67e98e0e6993f1dfbf70226f0327b
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-11-05 11:04:15 +0100

    add date formating

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,24 +1,28 @@
+import { format } from 'date-fns/esm';
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = ({items}) => {
-  return <BaseTable>
-  <THead>
-    <tr>
-      <Th>№</Th>
-      <Th>PRICE</Th>
-      <Th>AMOUNT</Th>
-      <Th>DATE</Th>
-    </tr>
-  </THead>
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+      <THead>
+        <tr>
+          <Th>№</Th>
+          <Th>PRICE</Th>
+          <Th>AMOUNT</Th>
+          <Th>DATE</Th>
+        </tr>
+      </THead>
 
-  <tbody>
-    {items.map(({id, price, amount, date})=>    <Tr>
-      <Td>{id}</Td>
-      <Td>{price}</Td>
-      <Td>{amount}</Td>
-      <Td>{date}</Td>
-    </Tr>)}
-  
-  </tbody>
-</BaseTable>
+      <tbody>
+        {items.map(({ id, price, amount, date }) => (
+          <Tr>
+            <Td>{id}</Td>
+            <Td>{price}</Td>
+            <Td>{amount}</Td>
+            <Td>{format(new Date(date), "yyyy-MM-dd 'Time:'HH:mm:ss.xxx")}</Td>
+          </Tr>
+        ))}
+      </tbody>
+    </BaseTable>
+  );
 };
